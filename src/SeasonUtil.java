50326572b5ab41ee06023de4f348c97210acd23f
Yonatan Dahary <yonatan.dahary@gmail.com>
2023-02-20 14:53:18 +0200

    SeasonUtil

--- src/SeasonUtil.java
+++ src/SeasonUtil.java
@@ -5,10 +5,15 @@ public class SeasonUtil {
     public static final Season[] SEASONS = Season.values();
     public static void sortSeasonable(Comparable[] seasonables) {
         // TODO: Implement.
+        Arrays.sort(seasonables);
     }
 
     static String reportAll(Seasonable[] seasonables) {
         // TODO: Implement.
-        return null;
+        String report = "";
+        for (Seasonable seasonable : seasonables) {
+            report += seasonable.toString()+ "\n";
+        }
+        return report;
     }
 }
